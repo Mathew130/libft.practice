b1257fd11a7c36ffb252a7b11b094910f183942c
Mathew130 <emilmathewlucka@gmail.com>
2026-04-26 20:43:20 +0000

    strlen function

---
+++ ft_strlen.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int ft_strlen(char *str)
+{    
+    int i;
+        i = 0;
+    while(str[i] !='\0') 
+    {
+        i++;
+    } 
+    return(i);
+}
+int main()
+{
+    char *s = "Mathew";
+    printf("%d\n",ft_strlen(s));
+}      
+
