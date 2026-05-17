f0f3880aa3259d6ca5c1dc50c4b5dad2a51d05aa
Mathew130 <emilmathewlucka@gmail.com>
2026-05-17 16:45:54 +0000

    trimfunction

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -28,8 +28,10 @@ char	*ft_strtrim(const char *s1, char const *set)
 	size_t	start;
 	size_t	end;
 	size_t	len;
-	char	*trimmed;
-
+  if(!s1 || !set)
+  return(NULL);
+  if(ft_strlen(s1) == 0)
+  return(ft_substr(s1, 0, 0));
 	start = 0;
 	end = ft_strlen(s1) - 1;
 	while (s1[start] && is_set(s1[start], set))
@@ -38,4 +40,6 @@ char	*ft_strtrim(const char *s1, char const *set)
 		end--;
 	if (start > end)
 		return (ft_substr(s1, 0, 0));
+    len = end - start + 1;
+    return(ft_substr(s1, start, len));
 }
